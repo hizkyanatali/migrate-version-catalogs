9aa63055d44f636fcf4e42431116da486d472ede
Hizkya <hizkyanatali@gmail.com>
2023-08-31 21:03:06 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    plugins {
+        `kotlin-dsl`
+    }
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "Migrate Version Catalogs"
+include(":app")
